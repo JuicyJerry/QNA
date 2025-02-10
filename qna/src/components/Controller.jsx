5324e52565931b4ce5c08cce60bf013b66c89c1c
Lee Jeong Hwan <ljh8842@gmail.com>
2025-02-10 23:54:26 +0900

    feat: Add page routing, style: Add initial page UI(not including design style)

---
+++ qna/src/components/Controller.jsx
@@ -0,0 +1,61 @@
+import "./Controller.css";
+import { useState, useRef } from "react";
+
+const Controller = ({ updateData, onClickButton }) => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  return (
+    <div className="controller">
+      <h3>controller 페이지</h3>
+      <div className="question">
+        <label htmlFor="question">Question: </label>
+        <input
+          onChange={(e) => setQuestion(e.target.value)}
+          type="text"
+          name="question"
+          id="question"
+        />
+        {question.length < 5 ? (
+          <span className="warning">5글자 이상 입력해주세요</span>
+        ) : (
+          <span></span>
+        )}
+      </div>
+      <div className="answer">
+        <label htmlFor="answer">Answer: </label>
+        <textarea
+          onChange={(e) => setAnswer(e.target.value)}
+          id="answer"
+          name="answer"
+          rows={5}
+          cols={33}
+        ></textarea>
+        {answer.length < 5 ? (
+          <span className="warning">5글자 이상 입력해주세요</span>
+        ) : (
+          <span></span>
+        )}
+      </div>
+      <div className="save">
+        <button
+          onClick={() => {
+            console.log(question.length);
+            console.log(answer.length);
+            if (question.length >= 5 && answer.length >= 5) {
+              onClickButton(+1);
+              updateData({ question: question, answer: answer });
+            } else {
+              alert("먼저, Question과 Answer를 입력해주세요.");
+            }
+          }}
+          type="button"
+        >
+          추가
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Controller;
